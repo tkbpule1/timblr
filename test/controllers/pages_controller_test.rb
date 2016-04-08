48aa5068bb59f750fd64994af8bf47bec5989f7e
Tim <tkbpule1@gmail.com>
2016-04-08 14:44:06 -0400

    Update pages controller test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample Application"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Sample App"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
