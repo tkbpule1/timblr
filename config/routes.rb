c07a35fd92278223151416ae90b1760e6352f7a4
Tim <tkbpule1@gmail.com>
2016-04-12 14:14:19 -0400

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
-  get 'help' => 'pages#help'
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
-  get 'signup' => 'users#new'
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+  root                'pages#home'
+  get 'help' =>       'pages#help'
+  get 'about' =>      'pages#about'
+  get 'contact' =>    'pages#contact'
+  get 'signup' =>     'users#new'
+  get 'login' =>      'sessions#new'
+  post 'login' =>     'sessions#create'
+  delete 'logout' =>  'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
