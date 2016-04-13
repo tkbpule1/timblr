c41db3351bd6c413e555a31419bdb54be4ac4a9d
Tim <tkbpule1@gmail.com>
2016-04-13 19:50:31 -0400

    fix microposts

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -10,7 +10,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    store :fog
+    storage :fog
   else
     storage :file
   end
